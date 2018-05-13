da5ae79f11dc8f575b73d0e23cc3265b808c8758
Trevor Livingston <tlivings@gmail.com>
2018-05-13 18:10:03 -0500

    use CBuffer (circular buffer) to keep size of samples within a limit

--- test/stats.js
+++ test/stats.js
@@ -56,15 +56,15 @@ test('sample', function (t) {
 
     stats.sample('foo', 10);
     t.ok('foo' in stats._samples);
-    t.equal(stats._samples.foo[0], 10);
+    t.equal(stats._samples.foo.get(0), 10);
 
     stats.sample('foo', 11);
     t.ok('foo' in stats._samples);
-    t.equal(stats._samples.foo[1], 11);
+    t.equal(stats._samples.foo.get(1), 11);
 
     stats.sample('bar', 12);
     t.ok('bar' in stats._samples);
-    t.equal(stats._samples.bar[0], 12);
+    t.equal(stats._samples.bar.get(0), 12);
 
     t.end();
 });
@@ -76,17 +76,17 @@ test('maxSamples', function (t) {
     t.equal(Object.keys(stats._samples).length, 0);
 
     stats.sample('foo', 10);
-    t.equal(stats._samples.foo.length, 1);
+    t.equal(stats._samples.foo.toArray().length, 1);
     t.ok('foo' in stats._samples);
-    t.equal(stats._samples.foo[0], 10);
+    t.equal(stats._samples.foo.get(0), 10);
 
     stats.sample('foo', 11);
-    t.equal(stats._samples.foo.length, 2);
+    t.equal(stats._samples.foo.toArray().length, 2);
     t.ok('foo' in stats._samples);
-    t.equal(stats._samples.foo[1], 11);
+    t.equal(stats._samples.foo.get(1), 11);
 
     stats.sample('foo', 12);
-    t.equal(stats._samples.foo.length, 2);
+    t.equal(stats._samples.foo.toArray().length, 2);
 
     t.end();
 });
@@ -99,9 +99,9 @@ test('maxSamplesDefault', function (t) {
     for (i = 0; i < 1000; i++) {
         stats.sample('foo', i);
     }
-    t.equal(stats._samples.foo.length, 1000);
+    t.equal(stats._samples.foo.toArray().length, 1000);
     stats.sample('foo', 1001);
-    t.equal(stats._samples.foo.length, 1000);
+    t.equal(stats._samples.foo.toArray().length, 1000);
 
     t.end();
 });
@@ -117,7 +117,7 @@ test('reset', function (t) {
 
     stats.sample('foo', 10);
     t.ok('foo' in stats._samples);
-    t.equal(stats._samples.foo[0], 10);
+    t.equal(stats._samples.foo.get(0), 10);
 
     stats.reset();
     t.equal(stats._counts.foo, 0);
